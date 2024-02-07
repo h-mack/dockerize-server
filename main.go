08f0af8bb29b678fecfcc54c36c955fffa5c56d2
Hamish Mackintosh-Keiller <h.mack.keiller@gmail.com>
2024-02-07 10:18:58 +1000

    Add middlewareCors function

--- main.go
+++ main.go
@@ -9,18 +9,20 @@ import (
 
 func main() {
 	m := http.NewServeMux()
+	corsMux := middlewareCors(m)
 
 	const addr = ":8000"
 
 	m.HandleFunc("/", handlePage)
 
 	srv := http.Server{
-		Handler:      m,
+		Handler:      corsMux,
 		Addr:         addr,
 		WriteTimeout: 30 * time.Second,
 		ReadTimeout:  30 * time.Second,
 	}
 
+	// this will block forever, until the server has an unrecoverable error
 	fmt.Println("server started on port ", addr)
 	err := srv.ListenAndServe()
 	log.Fatal(err)
@@ -38,4 +40,17 @@ func handlePage(w http.ResponseWriter, r *http.Request) {
 	`
 	w.WriteHeader(200)
 	w.Write([]byte(page))
+}
+
+func middlewareCors(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Access-Control-Allow-Origin", "*")
+		w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE")
+		w.Header().Set("Access-Control-Allow-Headers", "*")
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+		next.ServeHTTP(w, r)
+	})
 }
